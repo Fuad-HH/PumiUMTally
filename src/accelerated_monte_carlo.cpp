e8a331c3d92ca048f218f59c73c3a1247651aaae
Fuad Hasibul Hasan <fuadhhasan.acc@gmail.com>
2025-04-30 19:54:33 -0400

    added the main binary

---
+++ src/accelerated_monte_carlo.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Fuad Hasan on 4/30/25.
+//
+
+#include "physics/PumiTransport.h"
+#include "pumitallyopenmc/pumipic_particle_data_structure.h"
+#include <iostream>
+
+int main(int argc, char *argv[]) { return 0; }
